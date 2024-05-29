8e2415105d9ef9c91bb5c875cfa9b0d23d65545e
Chito <chitochi@proton.me>
2024-05-29 15:44:10 +0200

    Docker

--- flake.nix
+++ flake.nix
@@ -24,38 +24,68 @@
         abiVersions = ["armeabi-v7a" "arm64-v8a"];
       };
       androidSdk = androidComposition.androidsdk;
+      pythonPkgs = with pkgs.python3Packages; [
+        django_5
+        (django-ninja.override {
+          django = django_5;
+        })
+        (django-environ.override {
+          django = django_5;
+        })
+        pillow
+      ];
     in {
+      packages = {
+        backend = pkgs.python3Packages.buildPythonApplication rec {
+          pname = "rostrenenetmoi";
+          version = "1.0";
+          src = ./backend;
+          doCheck = false;
+
+          propagatedBuildInputs = pythonPkgs;
+
+          passthru = {
+            pythonPath = pkgs.python3Packages.makePythonPath propagatedBuildInputs;
+          };
+        };
+
+        backendDockerImage = let
+          backend = self.packages."${system}".backend;
+        in
+          pkgs.dockerTools.buildLayeredImage {
+            name = "backend";
+            tag = "latest";
+            contents = [backend pkgs.python3Packages.gunicorn];
+            config = {
+              Env = ["PYTHONPATH=${backend.pythonPath}"];
+              Cmd = ["gunicorn" "--bind" "0.0.0.0:8000" "--chdir" pkgs.python3.sitePackages "rostrenenetmoi.wsgi"];
+            };
+          };
+      };
+
       devShell = with pkgs;
         mkShell rec {
-          packages = [
-            # app dependencies
-            flutter
-            androidSdk
-            jdk17
-            clang
-            cmake
-            git
-            ninja
-            pkg-config
-            xz
-            gtk3
-            glib
-            pcre
+          packages =
+            [
+              # app dependencies
+              flutter
+              androidSdk
+              jdk17
+              clang
+              cmake
+              git
+              ninja
+              pkg-config
+              xz
+              gtk3
+              glib
+              pcre
 
-            # backend dependencies
-            ruff
-            (python3.withPackages (python-pkgs:
-              with python-pkgs; [
-                django_5
-                (django-ninja.override {
-                  django = django_5;
-                })
-                (django-environ.override {
-                  django = django_5;
-                })
-                pillow
-              ]))
-          ];
+              # backend dependencies
+              ruff
+              python3
+            ]
+            ++ pythonPkgs;
 
           ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
           GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/31.0.0/aapt2";

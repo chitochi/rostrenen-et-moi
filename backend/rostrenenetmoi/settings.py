ffc3a70edaf8ef053f262c6cdc771bbdb6ddd7cf
Chito <chitochi@proton.me>
2024-05-30 19:20:17 +0200

    Ajout d’une variable d’environnement

--- backend/rostrenenetmoi/settings.py
+++ backend/rostrenenetmoi/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = env("STATIC_ROOT", default=BASE_DIR / "staticfiles")
 
 STATIC_URL = "static/"
 
